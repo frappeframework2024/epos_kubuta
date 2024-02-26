864316ffb308ee0e6682b0e2c23e31979bf170b8
ratha7129 <tyrosa10@gmail.com>
2024-02-26 02:03:57 +0000

    update

--- epos_restaurant_2023/public/emenu/registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/assets/epos_restaurant_2023/emenu/sw.js', { scope: '/assets/epos_restaurant_2023/emenu/' })})}
