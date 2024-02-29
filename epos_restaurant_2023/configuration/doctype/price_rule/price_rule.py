89f47774965d425ea51b26754a6d022d02d3c3c4
ratha7129 <tyrosa10@gmail.com>
2024-02-29 09:27:27 +0000

    update 2024-02-29 4:27 PM

--- epos_restaurant_2023/configuration/doctype/price_rule/price_rule.py
+++ epos_restaurant_2023/configuration/doctype/price_rule/price_rule.py
@@ -1,8 +1,16 @@
 # Copyright (c) 2022, Tes Pheakdey and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class PriceRule(Document):
-	pass
+	def after_rename(self, olddn, newdn, merge=False):
+		frappe.db.sql("update `tabProduct` set prices = replace(price,N'{0}',N'{1}')".format(olddn,newdn))
+
+	def before_rename(self, olddn, newdn, merge=False):
+		existed = frappe.db.exists("Price Rule",self.name)
+		if existed :
+			doc = frappe.get_doc("Price Rule",self.name)
+			doc.previous_name = olddn
+			doc.save()
