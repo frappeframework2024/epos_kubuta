8eb2e08751225419157f85a4a81b3d890a35f853
ratha7129 <tyrosa10@gmail.com>
2024-03-02 03:08:41 +0000

    update 2024-03-02 10:08 AM

--- epos_restaurant_2023/configuration/doctype/price_rule/price_rule.py
+++ epos_restaurant_2023/configuration/doctype/price_rule/price_rule.py
@@ -6,7 +6,7 @@ from frappe.model.document import Document
 
 class PriceRule(Document):
 	def after_rename(self, olddn, newdn, merge=False):
-		frappe.db.sql("update `tabProduct` set prices = replace(price,N'{0}',N'{1}')".format(olddn,newdn))
+		frappe.enqueue("epos_restaurant_2023.configuration.doctype.price_rule.price_rule.update_product_prices", queue='long', self=self)
 
 	def before_rename(self, olddn, newdn, merge=False):
 		existed = frappe.db.exists("Price Rule",self.name)
@@ -14,3 +14,7 @@ class PriceRule(Document):
 			doc = frappe.get_doc("Price Rule",self.name)
 			doc.previous_name = olddn
 			doc.save()
+
+@frappe.whitelist()
+def update_product_prices(self):
+	frappe.db.sql("UPDATE `tabProduct` a SET a.prices = ((SELECT CONCAT('[', GROUP_CONCAT(JSON_OBJECT('price', price, 'branch', business_branch, 'price_rule', price_rule, 'portion', b.portion, 'unit', unit, 'default_discount', default_discount)),']') FROM `tabProduct Price` b WHERE b.parent = a.name))")
